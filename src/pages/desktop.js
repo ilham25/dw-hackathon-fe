de2b0997780a2f2e725547c177804839a1016b10
AhmadAfifM <ahmad.afifm@gmail.com>
2022-07-16 21:36:21 +0700

    add file manager

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 import backgroundDefault from "../assets/aether-dark.svg";
 import Taskbar from "../component/taskbar";
 import ModalNoteEdit from "../component/modal/modalNoteEdit";
+import ModalFileManager from "../component/modal/modalFileManager";
 
 export default function Desktop() {
   const [showNote, setShowNote] = useState(false);
+  const [showFileManager, setShowFileManager] = useState(false);
 
   return (
     <div
       className="w-full h-screen overflow-hidden "
       style={{ backgroundImage: `url(${backgroundDefault})` }}
     >
-      {showNote ? (
-        <ModalNoteEdit handleMinClose={() => setShowNote(false)} />
-      ) : (
-        <div className="h-full" />
+      {showNote && <ModalNoteEdit handleMinClose={() => setShowNote(false)} />}
+      {!showFileManager && <div className="h-full" />}
+
+      {showFileManager && (
+        <ModalFileManager handleMinClose={() => setShowFileManager(false)} />
       )}
-      <Taskbar showNoteEdit={() => setShowNote(!showNote)} />
+      <Taskbar
+        showNoteEdit={() => setShowNote(!showNote)}
+        showFileManager={() => setShowFileManager(!showFileManager)}
+      />
     </div>
   );
 }
